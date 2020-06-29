b26930aeb901b7246cac9ab36f3a0fc37c80abc0
justinvail <61876815+justinvail@users.noreply.github.com>
2020-06-29 13:56:16 -0400

    removing post and updating json

--- src/main/java/org/smartinrubio/springbootdynamodb/controller/HotelController.java
+++ src/main/java/org/smartinrubio/springbootdynamodb/controller/HotelController.java
@@ -37,20 +37,6 @@ public class HotelController {
         return ResponseEntity.ok("Data Loaded");
     }
 
-    @PostMapping
-    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel, UriComponentsBuilder uriComponentsBuilder) {
-        Hotel savedHotel = repository.save(hotel);
-        HttpHeaders headers = new HttpHeaders();
-        URI locationUri = uriComponentsBuilder
-                .path("/hotels/")
-                .path(String.valueOf(savedHotel.getId()))
-                .build()
-                .toUri();
-        headers.setLocation(locationUri);
-
-        return new ResponseEntity<>(savedHotel, headers, HttpStatus.CREATED);
-    }
-
     @GetMapping("/{hotelId}")
     public Hotel readHotelById(@PathVariable("hotelId") String id) {
         return repository.findById(id).orElseThrow(HotelNotFoundException::new);
@@ -59,15 +45,6 @@ public class HotelController {
     @GetMapping
     public List<Hotel> readHotelByName(@RequestParam("hotelName") String name) {
         return repository.findAllByName(name);
-    }
-
-    @PutMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Hotel updateHotel(@RequestBody Hotel hotel) {
-
-        repository.findById(hotel.getId()).orElseThrow(HotelNotFoundException::new);
-
-        return repository.save(hotel);
     }
 
     @DeleteMapping("/{hotelId}")
